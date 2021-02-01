b517b04ea80c012d043715c6e08fe33b5a7f2869
RafaGates <rafael12silva73@gmail.com>
2021-02-01 16:51:15 -0300

    visualizar notas

--- boletim/views.py
+++ boletim/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
+from timeline.forms import PublicarForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from models import Aluno, Nota
 
-# Create your views here.
+
+def ListNotas(request, aluno_id):
+    aluno = Aluno.objects.filter(id=aluno_id)
+    boletim = Nota.objects.filter(alunoID)
+
+    return render(request, 'notasAlunos.html',
+                  {'boletim': boletim})
+
+
